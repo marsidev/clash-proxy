d1761dcb773ed3d2e46b6927f9cc9df61aba1589
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-02-19 00:34:18 -0400

    get ip before request a new token

--- middlewares/setHeader.js
+++ middlewares/setHeader.js
@@ -1,26 +1,55 @@
 require('dotenv').config()
 const axios = require('axios')
 
+let localStorage
+
+if (typeof localStorage === 'undefined' || localStorage === null) {
+  const LocalStorage = require('node-localstorage').LocalStorage
+  localStorage = new LocalStorage('./scratch')
+}
+
+const getIP = async () => {
+  const url = 'https://api.ipify.org/'
+  const response = await axios.get(url)
+  return response.data
+}
+
 const setAuthHeader = async (req, res, next) => {
   try {
-    const { CR_API_TOKENER_URL, CRDEV_EMAIL, CRDEV_PASSWORD } = process.env
+    const { CR_API_TOKENER_URL, CRDEV_EMAIL, CRDEV_PASSWORD, CR_API_TOKENER_WHITELIST } = process.env
+
+    if (!CR_API_TOKENER_URL || !CRDEV_EMAIL || !CRDEV_PASSWORD || !CR_API_TOKENER_WHITELIST) {
+      throw new Error('Missing environment variables')
+    }
+
+    const ip = await getIP()
 
     const tokenerPayload = {
+      game: 'clashroyale',
       email: CRDEV_EMAIL,
       password: CRDEV_PASSWORD,
-      game: 'clashroyale',
-      whiteLlst: ['RoyaleAPI Proxy']
+      whiteLlst: CR_API_TOKENER_WHITELIST,
+      fixedIp: ip
     }
 
     const response = await axios.post(CR_API_TOKENER_URL, tokenerPayload)
-    // console.log(response.data)
+    // console.log({ response: response.data })
 
     const token = response.data.key
+
+    const localToken = localStorage.getItem('token')
+    if (localToken) {
+      console.log('local token found')
+      console.log({ localToken })
+    } else {
+      console.log('local token not found')
+      localStorage.setItem('token', token)
+    }
 
     req.headers.authorization = `Bearer ${token}`
     next()
   } catch (error) {
-    console.log(error.response.data)
+    console.log(error?.response?.data || error)
     next()
   }
 }
